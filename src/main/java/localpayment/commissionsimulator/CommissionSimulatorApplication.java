7c36b66f1b0bd4dbd593d400d3d20f3056816f48
nicolas cabral <nicolas.cabral@localpayment.com>
2020-12-10 11:49:06 -0300

    first commit

---
+++ src/main/java/localpayment/commissionsimulator/CommissionSimulatorApplication.java
@@ -0,0 +1,13 @@
+package localpayment.commissionsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/localpayment/commissionSimulator/CommissionSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package localpayment.commissionSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
